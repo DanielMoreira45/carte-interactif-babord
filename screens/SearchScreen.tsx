fa70a65a24cce25dd1827a06f2413cb459990942
khalil-45 <khalil.abada@etu.univ-orleans.fr>
2025-01-13 21:40:03 +0100

    🚧 #14 recherche sur les données recupérées fonctionnelles + ajout de quelques filtres

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -12,6 +12,7 @@ import {
   Dimensions,
 } from 'react-native';
 
+import ModalSelector from 'react-native-modal-selector';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {NavigationContainer, Link} from '@react-navigation/native';
@@ -27,6 +28,13 @@ const imageConcert = require('./assets/imageConcert.jpg');
 const SearchScreen = ({navigation}) => {
   const [rectangles, setRectangles] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [filters, setFilters] = useState({
+    type: 'groupes',
+    nb_homme: '0',
+    nb_femme: '0',
+    departement: '',
+  });
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('http://86.218.243.242:8000/api/groupes/', {
@@ -36,7 +44,7 @@ const SearchScreen = ({navigation}) => {
     })
       .then(response => response.json())
       .then(data => {
-        // Mettez à jour l'état des box des groupes avec les données récupérées
+        // Mettez à jour l'état des rectangles avec les données récupérées
         setRectangles(data);
       })
       .catch(error => {
@@ -44,21 +52,79 @@ const SearchScreen = ({navigation}) => {
       });
   }, []);
 
+  const applyFilters = () => {
+    // Ajoutez votre logique de filtrage ici
+    setModalVisible(false);
+  };
+
+  const resetFilters = () => {
+    setFilters({
+      type: 'groupes',
+      nb_homme: '0',
+      nb_femme: '0',
+      departement: '',
+    });
+  };
+
+  const typeOptions = [
+    { key: 'groupes', label: 'Groupes' },
+    { key: 'actualites', label: 'Actualités' },
+    { key: 'concerts', label: 'Concerts' },
+    { key: 'festivals', label: 'Festivals' },
+  ];
+
+  const numberOptions = [...Array(11).keys()].map(num => ({ key: num.toString(), label: num.toString() }));
+
+  const departmentOptions = [
+    { key: '01', label: 'Ain' },
+    { key: '02', label: 'Aisne' },
+    { key: '03', label: 'Allier' },
+    // Ajoutez tous les départements ici
+    { key: '75', label: 'Paris' },
+    { key: '76', label: 'Seine-Maritime' },
+    { key: '77', label: 'Seine-et-Marne' },
+    { key: '78', label: 'Yvelines' },
+    { key: '79', label: 'Deux-Sèvres' },
+    { key: '80', label: 'Somme' },
+    { key: '81', label: 'Tarn' },
+    { key: '82', label: 'Tarn-et-Garonne' },
+    { key: '83', label: 'Var' },
+    { key: '84', label: 'Vaucluse' },
+    { key: '85', label: 'Vendée' },
+    { key: '86', label: 'Vienne' },
+    { key: '87', label: 'Haute-Vienne' },
+    { key: '88', label: 'Vosges' },
+    { key: '89', label: 'Yonne' },
+    { key: '90', label: 'Territoire de Belfort' },
+    { key: '91', label: 'Essonne' },
+    { key: '92', label: 'Hauts-de-Seine' },
+    { key: '93', label: 'Seine-Saint-Denis' },
+    { key: '94', label: 'Val-de-Marne' },
+    { key: '95', label: 'Val-d\'Oise' },
+  ];
+
+  // Filtrer les groupes en fonction de la recherche
+  const filteredRectangles = rectangles.filter(item =>
+    item.libelle.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <ImageBackground source={background} style={[StyleSheet.absoluteFill]}>
       <FlatList
-        data={rectangles}
+        data={filteredRectangles}
         ListHeaderComponent={
           <View style={styles.searchContainer}>
             <View style={styles.searchInputContainer}>
               <Icon name="search" size={20} color="#888" style={styles.searchIcon} />
               <TextInput
                 style={styles.searchInput}
-                placeholder="Concerts, ..."
+                placeholder="Rechercher..."
                 placeholderTextColor="#888"
+                value={searchQuery}
+                onChangeText={text => setSearchQuery(text)}
               />
             </View>
-            <TouchableOpacity style={styles.filterButton} onPress={() => setModalVisible(true)}>
+            <TouchableOpacity style={styles.filterButton} onPress={() => setModalVisible(true)} testID="filter-button">
               <Icon name="filter" size={24} color="#888" />
             </TouchableOpacity>
           </View>
@@ -91,7 +157,66 @@ const SearchScreen = ({navigation}) => {
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Options de filtrage</Text>
-            {/* Ajoutez vos options de filtrage ici */}
+            <Text style={styles.filterLabel}>Type</Text>
+            <ModalSelector
+              data={typeOptions}
+              initValue="Sélectionner un type"
+              onChange={(option) => setFilters({ ...filters, type: option.key })}
+            >
+              <TextInput
+                style={styles.filterInput}
+                editable={false}
+                placeholder="Sélectionner un type"
+                value={typeOptions.find(option => option.key === filters.type)?.label}
+              />
+            </ModalSelector>
+            <Text style={styles.filterLabel}>Nombre d'hommes</Text>
+            <ModalSelector
+              data={numberOptions}
+              initValue="0"
+              onChange={(option) => setFilters({ ...filters, nb_homme: option.key })}
+            >
+              <TextInput
+                style={styles.filterInput}
+                editable={false}
+                placeholder="0"
+                value={filters.nb_homme}
+              />
+            </ModalSelector>
+            <Text style={styles.filterLabel}>Nombre de femmes</Text>
+            <ModalSelector
+              data={numberOptions}
+              initValue="0"
+              onChange={(option) => setFilters({ ...filters, nb_femme: option.key })}
+            >
+              <TextInput
+                style={styles.filterInput}
+                editable={false}
+                placeholder="0"
+                value={filters.nb_femme}
+              />
+            </ModalSelector>
+            <Text style={styles.filterLabel}>Département</Text>
+            <ModalSelector
+              data={departmentOptions}
+              initValue="Sélectionner un département"
+              onChange={(option) => setFilters({ ...filters, departement: option.key })}
+            >
+              <TextInput
+                style={styles.filterInput}
+                editable={false}
+                placeholder="Sélectionner un département"
+                value={departmentOptions.find(option => option.key === filters.departement)?.label}
+              />
+            </ModalSelector>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity style={styles.applyButton} onPress={applyFilters}>
+                <Text style={styles.applyButtonText}>Appliquer</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.resetButton} onPress={resetFilters}>
+                <Text style={styles.resetButtonText}>Réinitialiser</Text>
+              </TouchableOpacity>
+            </View>
             <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(!modalVisible)}>
               <Text style={styles.closeButtonText}>Fermer</Text>
             </TouchableOpacity>
@@ -229,6 +354,55 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 20,
+    fontFamily: 'Chivo-Regular',
+  },
+  filterLabel: {
+    width: '100%',
+    fontSize: 16,
+    fontFamily: 'Chivo-Regular',
+    marginBottom: 5,
+  },
+  filterPicker: {
+    width: '100%',
+    height: 40,
+    marginBottom: 10,
+  },
+  filterInput: {
+    width: '100%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    fontFamily: 'Chivo-Regular',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  applyButton: {
+    backgroundColor: '#ff3399',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginRight: 10,
+  },
+  applyButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'Chivo-Regular',
+  },
+  resetButton: {
+    backgroundColor: '#ccc',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  resetButtonText: {
+    color: '#000',
+    fontSize: 16,
     fontFamily: 'Chivo-Regular',
   },
   closeButton: {
